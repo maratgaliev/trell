050cdcf4cf8506e6928485d0fe7b8c7cdf1ff560
Marat Galiev <kazanlug@gmail.com>
2020-05-04 01:12:09 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# trell
+
+> Easy generate release ticket on Trello based on your git commits
+
+## Install
+
+```bash
+
+npm i -g trell
+
+```
+
+## Usage
+
+  
+```bash
+
+trell
+
+```
+
+![trell](https://api.monosnap.com/file/download?id=FAcUybSLLjdYyQpkcQJ4qiOPXEjJEE)
+
+## License
+
+[MIT](http://vjpr.mit-license.org)
---
+++ bin/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const clear = require('clear');
+const datefns = require('date-fns');
+const interactions = require('../lib/interactions');
+const Haikunator = require('haikunator');
+
+const figlet = require('figlet');
+const board = require('../lib/board');
+const repo = require('../lib/repo');
+
+var haikunator = new Haikunator({
+  defaults: {
+    tokenLength: 0
+  }
+})
+
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('TRELL', { horizontalLayout: 'full' })
+  )
+);
+
+const run = async () => {
+  const workingDir = await interactions.askGitDetails();
+  repo.getCommits(workingDir.repo).then(status => {
+    const descr = status.all.map((val) => { return val.message }).join('\n');
+    board.createRelease(datefns.format(new Date(), 'dd.MM.yyyy') + " - " + haikunator.haikunate(), descr);
+  });
+};
+
+run();
+
